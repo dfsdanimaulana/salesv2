eb34b26dff2ff7752354ecd4a75d8e3397280626
dfsdanimaulana <danimaulana9f@gmail.com>
2021-03-04 21:43:53 +0700

    add alert custom

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ $(function(){
   $('#btn_salesPerSift').click(function(){
     const sales = [$('#sales_1').val(), $('#sales_2').val(), $('#sales_3').val()];
     const cust = [$('#cust_1').val(), $('#cust_2').val(), $('#cust_3').val()];
-    if(sales.reduce((a,b)=>a+b)==''||cust.reduce((a,b)=>a+b)==''){
-      alert('please input your data!');
+    if(sales.reduce((a,b)=>a+b)==''&&cust.reduce((a,b)=>a+b)==''){
+      swal('Please input your data!','','warning');
+    }else if(sales.indexOf('')!=-1||cust.indexOf('')!=-1){
+      swal('Please complete your data!','','warning');
     }else{
       const sum_sales = eval(sales[0]+'+'+sales[1]+'+'+sales[2]);
       const sum_cust = eval(cust[0]+'+'+cust[1]+'+'+cust[2]);
@@ -15,8 +17,8 @@ $(function(){
       $('#area_salesPerSift').html(res);
       $('#copy_salesPerSift').toggleClass('visually-hidden');
     }
-    
   })
+  /**
   $('#copy_salesPerSift').click(function(){
     function copyText(){
     $('#area_salesPerSift').select().text();
@@ -24,7 +26,5 @@ $(function(){
     }
     copyText();
   })
+  **/
 });
-const rp = num => {
-    return num.toString().split('').reverse().join('').match(/\d{1,3}/g).join('.').split('').reverse().join('');
-  }
