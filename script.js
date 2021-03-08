93b634555b09b81d41d85abd0a00da31fd9b169f
dfsdanimaulana <danimaulana9f@gmail.com>
2021-03-08 21:36:43 +0700

    membuat fitur delete

--- script.js
+++ script.js
@@ -13,18 +13,27 @@ $(function(){
       const sum_sales = eval(sales[0]+'+'+sales[1]+'+'+sales[2]);
       const sum_cust = eval(cust[0]+'+'+cust[1]+'+'+cust[2]);
       const average = parseInt(sum_sales / sum_cust);
-      const res =`<p class='my-auto'>Rp.${rp(sum_sales)}</br>CC : ${rp(sum_cust)}</br>Avg : ${rp(average)}</p>`;
+      const res =`Rp.${rp(sum_sales)}</br>CC : ${rp(sum_cust)}</br>Avg : ${rp(average)}`;
       $('#area_salesPerSift').html(res);
-      $('#copy_salesPerSift').toggleClass('visually-hidden');
+      $('#reset_salesPerSift').toggleClass('visually-hidden');
     }
   })
-  /**
-  $('#copy_salesPerSift').click(function(){
-    function copyText(){
-    $('#area_salesPerSift').select().text();
-    document.execCommand('copy');
+  $('#btn_cashDrop').click(function(){
+    const cash = [$('#cash_1').val(), $('#cash_2').val(), $('#cash_3').val()];
+    if(cash.reduce((a,b)=>a+b)==''){
+      swal('Please input your data!','','warning');
+    }else if(cash.indexOf('')!=-1){
+      swal('Please complete your data!','','warning');
+    }else{
+      const sum_cash = rp(eval(cash[0]+'+'+cash[1]+'+'+cash[2]));
+      $('#area_cashDrop').text('Rp.'+sum_cash);
+      $('#reset_cashDrop').toggleClass('visually-hidden');
     }
-    copyText();
   })
-  **/
+  $('#reset_salesPerSift').click(function(){
+    $('#area_salesPerSift').html('');
+  })
+  $('#reset_cashDrop').click(function() {
+    $('#area_cashDrop').text('');
+  })
 });
